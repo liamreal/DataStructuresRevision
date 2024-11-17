df5a46fead3101c8316dd4486c9b6932d6545c1c
LiamFromGithubReal <liamkerrin@gmail.com>
2024-11-17 15:39:08 +0000

    completed course

--- graphs.py
+++ graphs.py
@@ -1,6 +1,8 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
+import os
+
 
 #
 
@@ -185,9 +187,14 @@ def draw_planar_graph_five_nodes():
     nx.draw_spring(graph, with_labels=True)
     plt.show()
 
-    # will readjust graph so edges dont intersect
-    nx.draw_planar(graph, with_labels=True)
-    plt.show()
+    # catch the error that will be thrown for this planar graph creation
+    try:
+        # will readjust graph so edges dont intersect
+        # (throws ERROR because this is not possible in complete graph with 5 nodes)
+        nx.draw_planar(graph, with_labels=True)
+        plt.show()
+    except Exception as error:
+        print(f'Complete Graph with 5 nodes cannot be drawn as planar!\n{error}')
 
 def draw_complete_graph():
     # specify number of nodes
@@ -410,62 +417,314 @@ def find_eulerian_path():
     nx.draw_spring(graph, with_labels=True)
     plt.show()
 
+def find_cliques_and_k_core():
+    # CREATE one graph (same one used in show_betweenness_centrality function)
+    graph_one = nx.complete_graph(5)
+    graph_two = nx.complete_graph(5)
+    graph_two = nx.relabel_nodes(graph_two, {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E'})
+    graph_connector = nx.from_edgelist([(4, 'X'), ('X', 'A')])
+    graph = nx.compose_all([graph_one, graph_two, graph_connector])
+
+    # Cliques of a graph are the subset (i.e. subgraphs) of complete graphs in our graph
+    # For example, in our above, [1,2,3,4,5] are all connected, so they are all complete, so
+    # this is one of our cliques, same applies for [4, 'X'], because they both are connected
+    # to each other, HOWEVER this would NOT apply to [1,2,3,4,5,'X'] BECAUSE 'X' is ONLY
+    # connected to 4 and not the rest, so it would not be complete, same applies for other
+    # side of the graph
+    print(f'\nCliques of example graph: \n\t{list(nx.find_cliques(graph))}')
+
+    # Can also do k-core, which is the maximal connect subgraph of G in which all vertices
+    # have degree of AT LEAST k (so each node has minimum of k connections)
+    for k in range(1, 6):
+        print(f'\nK-Core of example (k = {k}): \n\t{list(nx.k_core(graph, k))}')
+
+    # first lets show how it draws normally using spring
+    nx.draw_spring(graph, with_labels=True)
+    plt.show()
+
+def find_bridges():
+    # CREATE one graph (same one used in show_betweenness_centrality function)
+    graph_one = nx.complete_graph(5)
+    graph_two = nx.complete_graph(5)
+    graph_two = nx.relabel_nodes(graph_two, {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E'})
+    graph_connector = nx.from_edgelist([(4, 'X'), ('X', 'A')])
+    graph = nx.compose_all([graph_one, graph_two, graph_connector])
+
+    # Bridges are edges that, when deleted, increase the number of connected components,
+    # for example, our graph connectors above, (4, 'X') and ('X', 'A'), would be bridges,
+    # because by removing them you would be separating the graph into two different graphs
+    print(f'\nBridges of example graph: \n\t{list(nx.bridges(graph))}')
+
+    # A Local Bridge is just an edge connecting two nodes, removing it DOESNT mean it separates
+    # graph, it will just mean that in order to get from that node to the other, a longer path
+    # needs to be taken, it also returns the span of this local bridge (in our case inf)
+    # Since we have complete graphs either sides of our graph bridges, are also local bridges
+    print(f'\nLocal Bridges of example graph: \n\t{list(nx.local_bridges(graph))}')
+
+    # If we add an alternate bridge, our previous bridge will no longer be a global bridge,
+    # just a local bridge, and if we were to remove it, it would result in the longer path
+    # being taken
+    graph_updated = nx.compose_all([graph])
+    graph_updated.add_edge(4, 'F')
+    graph_updated.add_edge('F', 'G')
+    graph_updated.add_edge('G', 'H')
+    graph_updated.add_edge('H', 'A')
+    # no global bridges, graph is connected more than one way
+    print(f'\n\nBridges of updated graph: \n\t{list(nx.bridges(graph_updated))}')
+    # here if for example we remove edge:
+    #       (4, 'X')
+    # we will have to take path:
+    #       [4, 'F', 'G', 'H', 'A', 'X']
+    # to get to node 'X', and the span is 5 because had to take 5 edges as alternative
+    print(f'\nLocal Bridges of updated graph: \n\t{list(nx.local_bridges(graph_updated))}')
+
+    # first lets show how it draws normally using spring
+    nx.draw_spring(graph, with_labels=True)
+    plt.show()
+
+    # first lets show how it draws normally using spring
+    nx.draw_spring(graph_updated, with_labels=True)
+    plt.show()
+
+def find_connected_components():
+    # CREATE one graph (same one used in show_betweenness_centrality function)
+    graph_one = nx.complete_graph(5)
+    graph_two = nx.complete_graph(5)
+    graph_two = nx.relabel_nodes(graph_two, {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E'})
+    graph_connector = nx.from_edgelist([(4, 'X'), ('X', 'A')])
+    graph = nx.compose_all([graph_one, graph_two, graph_connector])
+
+    # this will list the sets of nodes connected to one another through some combination of edges,
+    # so in our case currently the whole graph is connected, so it will all be in one set
+    print(f'\nConnected Components of example graph: \n\t{list(nx.connected_components(graph))}')
+
+    # however, if we remove edge ('X', 'A'), our graph will be split in two, so will be two sets
+    graph_updated = nx.compose_all([graph])
+    graph_updated.remove_edge('X', 'A')
+    print(f'\nConnected Components of example graph: \n\t{list(nx.connected_components(graph_updated))}')
+
+    # first lets show how it draws normally using spring
+    nx.draw_spring(graph, with_labels=True)
+    plt.show()
+
+    # first lets show how it draws normally using spring
+    nx.draw_spring(graph_updated, with_labels=True)
+    plt.show()
+
+def find_directionally_connected_components():
+    # can also add edges as a list of tuples
+    edge_list = [('A', 'C'), ('B', 'C'), ('A', 'B'), ('B', 'A')]
+
+    # CREATE directional graph, add edges from our list
+    graph = nx.DiGraph()
+    graph.add_edges_from(edge_list)
+
+    # the graph we have looks like this:
+    #
+    #    A-->C
+    #   ^|  ^
+    #   |v /
+    #    B
+    #
+    # so basically:
+    #
+    #       A-->C, B-->C, A<-->B
+
+    # this means ('A', 'C') and ('B', 'C') are not strongly connected, BUT ('A', 'B') are
+    print(f'\nStrongly Connected Components of example graph: \n\t{list(nx.strongly_connected_components(graph))}')
+    # HOWEVER, ('A', 'C') and ('B', 'C') ARE weakly connected components, you can get
+    # from A or B to C, but you cannot get to A or B from C, if an edge in a graph is
+    # strongly connected, it is also by definition also weakly connected
+    print(f'\nWeakly Connected Components of example graph: \n\t{list(nx.weakly_connected_components(graph))}')
+
+    # however, if we added edges from C to A and B, they would become strongly connected
+    graph_updated = nx.compose_all([graph])
+    graph_updated.add_edge('C', 'A')
+    graph_updated.add_edge('C', 'B')
+
+    # getting connected components for updated graph
+    print(f'\nStrongly Connected Components of updated graph: \n\t{list(nx.strongly_connected_components(graph_updated))}')
+    # you will see that, since all our connected components fit criteria for strong, they
+    # also fit criteria for weak
+    print(f'\nWeakly Connected Components of updated graph: \n\t{list(nx.weakly_connected_components(graph_updated))}')
+
+
+    # first lets show how it draws normally using spring
+    nx.draw_spring(graph, with_labels=True)
+    plt.show()
+
+    # first lets show how it draws normally using spring
+    nx.draw_spring(graph_updated, with_labels=True)
+    plt.show()
+
+def program_loop():
+    run = True
+    user_input = -1
+
+    # run while user wishes to use program
+    while run:
+        # clear screen
+        os.system('cls')
+        print('\nWelcome to a brief overview of Graph Theory in Python \n\t(using the NetworkX library)')
+        print('\nHere are your options (1-20):\n\t(check out comments in each function for more in-depth information)')
+
+        print('\n\t(1): Simple Overview')
+        print('\t(2): Creating Graph from Edge List')
+        print('\t(3): Adding to Graph from Edge List')
+        print('\t(4): Get Adjacency Matrix')
+        print('\t(5): Create Adjacency Matrix')
+        print('\t(6): Draw Graphs')
+        print('\t(7): Draw Planar Graph with four nodes')
+        print('\t(8): Draw Planar Graph with five nodes (THROWS EXCEPTION)')
+        print('\t(9): Draw Complete Graph')
+        print('\t(10): Get Graph Degrees')
+        print('\t(11): Get Directed Graph Degrees')
+        print('\t(12): Find Shortest Path')
+        print('\t(13): Find Centrality')
+        print('\t(14): Show Betweenness Centrality')
+        print('\t(15): Get Density and Diameter')
+        print('\t(16): Find Eulerian Path')
+        print('\t(17): Find Cliques and K-Core')
+        print('\t(18): Find Bridges')
+        print('\t(19): Find Connected Components')
+        print('\t(20): Find Directionally Connected Components')
+
+        print('\nPress \'e\' to exit program')
+
+        # loop while user has not picked valid number or chosen to exit program
+        while (user_input not in range(1, 21)) or (user_input != 'E'.lower()):
+            user_input = input('\nChoose an input from list: ')
+
+            if (user_input.lower() == 'e'):
+                run = False
+                break
+            else:
+                try:
+                    user_input = int(user_input)
+                except:
+                    print('Invalid input')
+                    continue
+
+            # different cases for functions
+            match user_input:
+                case 1:
+                    # function gives simple overview of basic functionality of NetworkX
+                    simple_overview()
+                case 2:
+                    # using edge list to CREATE graph
+                    creating_from_edge_list()
+                case 3:
+                    # also using edge list, but ADDING instead of CREATING
+                    adding_from_edge_list()
+                case 4:
+                    # can also get the adjacency matrix representation of our graph
+                    get_adjacency_matrix()
+                case 5:
+                    # can also create adjacency matrix ourselves using numpy
+                    create_adjacency_matrix()
+                case 6:
+                    # lets look at different ways to draw the graphs
+                    draw_graphs()
+                case 7:
+                    # in relation to planar graphs, we can display a COMPLETE graph,
+                    # and see how for planar graphs it will try to avoid edges going
+                    # through other edges, lets try a complete graph with 4 nodes
+                    draw_planar_graph_four_nodes()
+                case 8:
+                    # now lets try a complete graph with 5 nodes
+                    # (will throw exception that G is not planar)
+                    draw_planar_graph_five_nodes()
+                case 9:
+                    # also, we dont have to manually generate complete graphs
+                    draw_complete_graph()
+                case 10:
+                    # we can get specific information about our graph
+                    get_graph_degrees()
+                case 11:
+                    # can also do the same SPECIFICALLY for directed graphs (in/out degrees)
+                    get_directed_graph_degrees()
+                case 12:
+                    # can also do the same SPECIFICALLY for directed graphs (in/out degrees)
+                    find_shortest_path()
+                case 13:
+                    # sometimes we want to know how central a node is in the graph, can decide in different ways
+                    # (all the different centralities used are explained through comments within the function)
+                    find_centrality()
+                case 14:
+                    # to show betweenness centrality a bit better, will showcase in its own function
+                    show_betweenness_centrality()
+                case 15:
+                    # can obtain density and diameter of graph
+                    get_density_and_diameter()
+                case 16:
+                    # can find the Eulerian path (explained in function), only visit each edge ONCE (if possible)
+                    find_eulerian_path()
+                case 17:
+                    # can find all cliques and k-core (explained in function)
+                    find_cliques_and_k_core()
+                case 18:
+                    # can find bridges
+                    find_bridges()
+                case 19:
+                    # can find connected components
+                    find_connected_components()
+                case 20:
+                    # can find strong/weak connected components of directed graphs
+                    find_directionally_connected_components()
+                case _:
+                    print('Invalid number entered')
+                    break
+            # this is to allow user to see results before continuing back to main menu, but do not need store input
+            input('\n--Press any key to continue to main menu--')
+            # reset user input and break out of this loop to get back to main menu loop
+            user_input = -1
+            break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
-    # # function gives simple overview of basic functionality of NetworkX
-    # simple_overview()
+    # entire program
+    program_loop()
 
-
-    # # using edge list to CREATE graph
-    # creating_from_edge_list()
-
-    # # also using edge list, but ADDING instead of CREATING
-    # adding_from_edge_list()
-
-
-    # # can also get the adjacency matrix representation of our graph
-    # get_adjacency_matrix()
-
-    # # can also create adjacency matrix ourselves using numpy
-    # create_adjacency_matrix()
-
-    # # lets look at different ways to draw the graphs
-    # draw_graphs()
-
-    # # in relation to planar graphs, we can display a COMPLETE graph,
-    # # and see how for planar graphs it will try to avoid edges going
-    # # through other edges, lets try a complete graph with 4 nodes
-    # draw_planar_graph_four_nodes()
-
-    # # now lets try a complete graph with 5 nodes
-    # # (will throw exception that G is not planar)
-    # draw_planar_graph_five_nodes()
-
-    # # also, we dont have to manually generate complete graphs
-    # draw_complete_graph()
-
-    # # we can get specific information about our graph
-    # get_graph_degrees()
-
-    # # can also do the same SPECIFICALLY for directed graphs (in/out degrees)
-    # get_directed_graph_degrees()
-
-    # # can also do the same SPECIFICALLY for directed graphs (in/out degrees)
-    # find_shortest_path()
-
-    # # sometimes we want to know how central a node is in the graph, can decide in different ways
-    # # (all the different centralities used are explained through comments within the function)
-    # find_centrality()
-
-    # # to show betweenness centrality a bit better, will showcase in its own function
-    # show_betweenness_centrality()
-
-    # # can obtain density and diameter of graph
-    # get_density_and_diameter()
-
-    # can find the Eulerian path (explained in function), only visit each edge ONCE (if possible)
-    find_eulerian_path()
-
+    print('END OF MAIN')
 
 
 
